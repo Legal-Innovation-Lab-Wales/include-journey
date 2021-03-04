07ebddd8e29db3c1e5d4f9e5370503f1054670ad
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-04 18:16:52 +0000

    IJ-31 Added mobile number

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
   # validations
   validates_presence_of :first_name,
                         :last_name,
+                        :mobile_number,
                         :email,
                         :terms
   validates :email, uniqueness: { case_sensitive: false }
