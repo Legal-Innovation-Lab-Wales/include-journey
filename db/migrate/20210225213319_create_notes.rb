34f1cc4e7a45ca0438f2ce149959a2b8a3d7c562
phil-6 <github@purpleriver.dev>
2021-03-05 11:05:27 +0000

    IJ-10 add note seed and note validations
    also update migration to ensure visible_to_user defaults to false

--- db/migrate/20210225213319_create_notes.rb
+++ db/migrate/20210225213319_create_notes.rb
@@ -2,7 +2,7 @@ class CreateNotes < ActiveRecord::Migration[6.1]
   def change
     create_table :notes do |t|
       t.text :content
-      t.boolean :visible_to_user
+      t.boolean :visible_to_user, null: false, default: false
       t.belongs_to :team_member, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,68 +10,73 @@ require 'faker'
 
 # Create users
 #Create Static Users
-team_member = TeamMember.new(
-  first_name: 'Phil',
-  last_name: 'Reynolds',
-  email: 'philr@purpleriver.dev',
-  mobile_number: '07888777666',
-  admin: true,
-  terms: true,
-  password: 'password'
+# team_member = TeamMember.new(
+#   first_name: 'Phil',
+#   last_name: 'Reynolds',
+#   email: 'philr@purpleriver.dev',
+#   mobile_number: '07888777666',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+# team_member = TeamMember.new(
+#   first_name: 'Alex',
+#   last_name: 'Wing',
+#   email: 'a.j.wing@swansea.ac.uk',
+#   mobile_number: '07777666555',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+# team_member = TeamMember.new(
+#   first_name: 'Ieuan',
+#   last_name: 'Skinner',
+#   email: 'ieuan.skinner@swansea.ac.uk',
+#   mobile_number: '07666555444',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+# team_member = TeamMember.new(
+#   first_name: 'Ben',
+#   last_name: 'Harrison',
+#   email: 'benmharrison@me.com',
+#   mobile_number: '07555444333',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+#
+# 10.times do
+#   user = User.new(
+#     first_name: Faker::Name.unique.first_name,
+#     last_name: Faker::Name.unique.last_name,
+#     email: Faker::Internet.unique.email,
+#     mobile_number: Faker::Number.leading_zero_number(digits: 11),
+#     release_date: rand(1..2).even? ? Faker::Date.between(from: '20201-03-05', to: '2025-03-05') : '',
+#     terms: true,
+#     password: 'password'
+#   )
+#   user.skip_confirmation!
+#   user.save!
+#
+# end
+
+Note.create!(
+  team_member_id: 1,
+  visible_to_user: true,
+  user_id: User.first.id,
+  content: Faker::TvShows::TheExpanse.quote
 )
-team_member.skip_confirmation!
-team_member.save!
-
-team_member = TeamMember.new(
-  first_name: 'Alex',
-  last_name: 'Wing',
-  email: 'a.j.wing@swansea.ac.uk',
-  mobile_number: '07777666555',
-  admin: true,
-  terms: true,
-  password: 'password'
-)
-team_member.skip_confirmation!
-team_member.save!
-
-team_member = TeamMember.new(
-  first_name: 'Ieuan',
-  last_name: 'Skinner',
-  email: 'ieuan.skinner@swansea.ac.uk',
-  mobile_number: '07666555444',
-  admin: true,
-  terms: true,
-  password: 'password'
-)
-team_member.skip_confirmation!
-team_member.save!
-
-team_member = TeamMember.new(
-  first_name: 'Ben',
-  last_name: 'Harrison',
-  email: 'benmharrison@me.com',
-  mobile_number: '07555444333',
-  admin: true,
-  terms: true,
-  password: 'password'
-)
-team_member.skip_confirmation!
-team_member.save!
-
-
-10.times do
-  user = User.new(
-    first_name: Faker::Name.unique.first_name,
-    last_name: Faker::Name.unique.last_name,
-    email: Faker::Internet.unique.email,
-    mobile_number: Faker::Number.leading_zero_number(digits: 11),
-    release_date: rand(1..2).even? ? Faker::Date.between(from: '20201-03-05', to: '2025-03-05') : '',
-    terms: true,
-    password: 'password'
-  )
-  user.skip_confirmation!
-  user.save!
-
-end
-
-
