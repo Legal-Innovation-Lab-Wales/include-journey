ca2bc1357dbcfe24c9262cbba83b139a61502449
phil-6 <github@purpleriver.dev>
2021-03-05 11:51:07 +0000

    IJ-10 seed one wba_self and fix model association

--- app/models/wba_self.rb
+++ app/models/wba_self.rb
@@ -1,7 +1,7 @@
 class WbaSelf < ApplicationRecord
   belongs_to :user
 
-  has_many :wba_self_scores, foreign_key: true
-  has_many :wba_self_permissions, foreign_key: true
-  has_many :wba_self_view_logs, foreign_key: true
+  has_many :wba_self_scores, foreign_key: :wba_self_id
+  has_many :wba_self_permissions, foreign_key: :wba_self_id
+  has_many :wba_self_view_logs, foreign_key: :wba_self_id
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,69 +10,69 @@ require 'faker'
 
 # Create users
 ## Create Static Users
-team_member = TeamMember.new(
-  first_name: 'Phil',
-  last_name: 'Reynolds',
-  email: 'philr@purpleriver.dev',
-  mobile_number: '07888777666',
-  admin: true,
-  terms: true,
-  password: 'password'
-)
-team_member.skip_confirmation!
-team_member.save!
-
-team_member = TeamMember.new(
-  first_name: 'Alex',
-  last_name: 'Wing',
-  email: 'a.j.wing@swansea.ac.uk',
-  mobile_number: '07777666555',
-  admin: true,
-  terms: true,
-  password: 'password'
-)
-team_member.skip_confirmation!
-team_member.save!
-
-team_member = TeamMember.new(
-  first_name: 'Ieuan',
-  last_name: 'Skinner',
-  email: 'ieuan.skinner@swansea.ac.uk',
-  mobile_number: '07666555444',
-  admin: true,
-  terms: true,
-  password: 'password'
-)
-team_member.skip_confirmation!
-team_member.save!
-
-team_member = TeamMember.new(
-  first_name: 'Ben',
-  last_name: 'Harrison',
-  email: 'benmharrison@me.com',
-  mobile_number: '07555444333',
-  admin: true,
-  terms: true,
-  password: 'password'
-)
-team_member.skip_confirmation!
-team_member.save!
-
-
-10.times do
-  user = User.new(
-    first_name: Faker::Name.unique.first_name,
-    last_name: Faker::Name.unique.last_name,
-    email: Faker::Internet.unique.email,
-    mobile_number: Faker::Number.leading_zero_number(digits: 11),
-    release_date: rand(1..2).even? ? Faker::Date.between(from: '20201-03-05', to: '2025-03-05') : '',
-    terms: true,
-    password: 'password'
-  )
-  user.skip_confirmation!
-  user.save!
-
-end
+# team_member = TeamMember.new(
+#   first_name: 'Phil',
+#   last_name: 'Reynolds',
+#   email: 'philr@purpleriver.dev',
+#   mobile_number: '07888777666',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+# team_member = TeamMember.new(
+#   first_name: 'Alex',
+#   last_name: 'Wing',
+#   email: 'a.j.wing@swansea.ac.uk',
+#   mobile_number: '07777666555',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+# team_member = TeamMember.new(
+#   first_name: 'Ieuan',
+#   last_name: 'Skinner',
+#   email: 'ieuan.skinner@swansea.ac.uk',
+#   mobile_number: '07666555444',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+# team_member = TeamMember.new(
+#   first_name: 'Ben',
+#   last_name: 'Harrison',
+#   email: 'benmharrison@me.com',
+#   mobile_number: '07555444333',
+#   admin: true,
+#   terms: true,
+#   password: 'password'
+# )
+# team_member.skip_confirmation!
+# team_member.save!
+#
+#
+# 10.times do
+#   user = User.new(
+#     first_name: Faker::Name.unique.first_name,
+#     last_name: Faker::Name.unique.last_name,
+#     email: Faker::Internet.unique.email,
+#     mobile_number: Faker::Number.leading_zero_number(digits: 11),
+#     release_date: rand(1..2).even? ? Faker::Date.between(from: '20201-03-05', to: '2025-03-05') : '',
+#     terms: true,
+#     password: 'password'
+#   )
+#   user.skip_confirmation!
+#   user.save!
+#
+# end
 
 Note.create!(
   team_member_id: 1,
@@ -82,59 +82,137 @@ Note.create!(
 )
 
 if WellbeingMetric.count.zero?
+  # id: 1
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Housing',
     category: 'Environment'
   )
+  # id: 2
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Benefits / Money',
     category: 'Environment'
   )
+  # id: 3
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Food',
     category: 'Environment'
   )
+  # id: 4
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Physical Health',
     category: 'Health'
   )
+  # id: 5
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Mental Health',
     category: 'Health'
   )
+  # id: 6
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Emotional Health',
     category: 'Health'
   )
+  # id: 7
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Behaviour',
     category: 'Personal'
   )
+  # id: 8
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Addiction',
     category: 'Health'
   )
+  # id: 9
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Relationships',
     category: 'Personal'
   )
+  # id: 10
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Sense of Community ',
     category: 'Personal'
   )
+  # id: 11
   WellbeingMetric.create!(
     team_member_id: 1,
     name: 'Employment/Education/Training',
     category: 'Personal'
   )
 end
+
+WbaSelf.create!(
+
+  # id: 1
+  user_id: User.first.id
+)
+
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 1, # housing
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 2, # Benefits / Money
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 3,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 4,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 5,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 6,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 7,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 8,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 9,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 10,
+  value: 8
+)
+WbaSelfScore.create!(
+  wba_self_id: 1,
+  wellbeing_metric_id: 11,
+  value: 8
+)
+
+WbaSelfPermission.create!(
+  wba_self_id: 1,
+  team_member_id: 1
+)
