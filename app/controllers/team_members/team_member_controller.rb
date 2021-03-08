9ce9b00b67298c7458a6c7f29793daab6de7e5da
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-08 15:20:30 +0000

    IJ-64 Added controller configuration so that team members will not be able to access the system unless they are approved.

--- app/controllers/team_members/team_member_controller.rb
+++ app/controllers/team_members/team_member_controller.rb
@@ -2,5 +2,15 @@ module TeamMembers
   # app/controllers/team_members/team_member_controller.rb
   class TeamMemberController < ApplicationController
     before_action :authenticate_team_member!
+    before_action :require_approval
+
+    protected
+
+    def require_approval
+      if current_team_member.present? && !current_team_member.approved?
+        sign_out_and_redirect(current_team_member)
+        session[:awaiting_approval_notice] = 'An admin needs to approve you before you can access the system!'
+      end
+    end
   end
 end
