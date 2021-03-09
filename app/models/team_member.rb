75218d6a49dee7f0e4ab8a48681818e379fcccb1
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-09 11:03:00 +0000

    IJ-42 Individual team member view will now show their details including metadata about when their account was created, and sign in info.

--- app/models/team_member.rb
+++ app/models/team_member.rb
@@ -24,6 +24,18 @@ class TeamMember < ApplicationRecord
     self.last_sign_in_at.present? ? self.last_sign_in_at.strftime("%d/%m/%Y %I:%M %p") : ''
   end
 
+  def get_current_sign_in
+    self.current_sign_in_at.present? ? self.current_sign_in_at.strftime("%d/%m/%Y %I:%M %p") : ''
+  end
+
+  def get_created_at
+    self.created_at.strftime("%d/%m/%Y %I:%M %p")
+  end
+
+  def last_update
+    self.updated_at.strftime("%d/%m/%Y %I:%M %p")
+  end
+
   private
 
   # validations
