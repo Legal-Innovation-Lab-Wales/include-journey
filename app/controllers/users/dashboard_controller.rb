ae15c1c10c4b85400560ad1e1aefc144edb542df
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-10 15:31:58 +0000

    IJ-47 Fix so that the SOS modal can be filled out no matter the parent view.

--- app/controllers/users/dashboard_controller.rb
+++ app/controllers/users/dashboard_controller.rb
@@ -3,16 +3,8 @@
 module Users
   # app/controllers/users/dashboard_controller.rb
   class DashboardController < UsersApplicationController
-    before_action :crisis_event, only: :main
-
     def main
       render template: 'users/dashboard/main'
-    end
-
-    private
-
-    def crisis_event
-      @crisis_event = CrisisEvent.new
     end
   end
 end
