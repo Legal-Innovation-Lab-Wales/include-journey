243d1154166a6c1302c47a2b4c67d1fc4a69609e
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-11 16:57:58 +0000

    IJ-50 Added validation to the permissions so that a user must select at least one team member their wellbeing assessment can be shared with.

--- app/controllers/users/wba_selves_controller.rb
+++ app/controllers/users/wba_selves_controller.rb
@@ -5,7 +5,7 @@ module Users
   class WbaSelvesController < UsersApplicationController
     before_action :wellbeing_metrics, :wba_selves_params, only: :create
     after_action :create_wba_self_scores, only: :create
-    before_action :wba_self, :team_members, :wba_self_permissions_params, :create_wba_self_permissions, only: :permissions
+    before_action :wba_self, :team_members, :wba_self_permissions_params, :validate_wba_self_permissions_params, :create_wba_self_permissions, only: :permissions
 
     # POST /wba_selves/create
     def create
@@ -29,7 +29,7 @@ module Users
 
     def create_wba_self_permissions
       @team_members.each do |team_member|
-        next unless wba_self_permissions_params[team_member_id(team_member.id)] == '1'
+        next unless @wba_self_permissions_params[team_member_id(team_member.id)] == '1'
 
         unless WbaSelfPermission.create!({ wba_self_id: @wba_self.id, team_member_id: team_member.id })
           puts("WBA Self Permission for user #{team_member.email} was not recorded for WBA Self #{@wba_self.id}")
@@ -60,11 +60,17 @@ module Users
     end
 
     def wba_self_permissions_params
-      params.require(:wba_self).permit(@team_members.map { |team_member| team_member_id(team_member.id) })
+      @wba_self_permissions_params = params.require(:wba_self).permit(@team_members.map { |team_member| team_member_id(team_member.id) })
     end
 
     def wba_selves_params
       params.require(:wba_self).permit(@wellbeing_metrics.map { |metric| metric_id(metric.id) })
+    end
+
+    def validate_wba_self_permissions_params
+      return if @team_members.map { |team_member| @wba_self_permissions_params[team_member_id(team_member.id)] }.include? '1'
+
+      redirect_to authenticated_user_root_path, alert: 'You must select at least one team member!'
     end
 
     def metric_id(metric_id)
