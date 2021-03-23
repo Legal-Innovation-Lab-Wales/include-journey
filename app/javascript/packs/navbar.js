5ded556cce9e625831bb26f92330ec9ff21d33ce
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-23 10:38:03 +0000

    IJ-79 Removed redundant JS line.

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -2,12 +2,6 @@ const buttons = document.querySelectorAll('.navbar .btn')
 
 buttons.forEach(btn => {
     const label = btn.querySelector('.nav-label')
-    btn.addEventListener('mouseenter', () => {
-        label.classList.add('show')
-        label.classList.remove('hide')
-    })
-    btn.addEventListener('mouseleave', () => {
-        label.classList.remove('show')
-        label.classList.add('hide')
-    })
+    btn.addEventListener('mouseenter', () => label.classList.add('show'))
+    btn.addEventListener('mouseleave', () => label.classList.remove('show'))
 })
