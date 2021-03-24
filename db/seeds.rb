aa2ad983c666a363921dc97fa34961578bbcfc69
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-24 16:06:13 +0000

    IJ-111 Updated seed to use less fixed assignments.

--- db/seeds.rb
+++ db/seeds.rb
@@ -77,7 +77,7 @@ if User.count.zero?
     user = User.new(
       first_name: Faker::Name.unique.first_name,
       last_name: Faker::Name.unique.last_name,
-      email: 'IJ-test-user-'+user_count.to_s+'@purpleriver.dev',
+      email: "IJ-test-user-#{user_count}@purpleriver.dev",
       mobile_number: Faker::Number.leading_zero_number(digits: 11),
       release_date: rand(1..2).even? ? Faker::Date.between(from: '2021-03-05', to: '2025-03-05') : '',
       terms: true,
@@ -86,68 +86,80 @@ if User.count.zero?
     user.skip_confirmation!
     user.save!
   end
+
+  user = User.new(
+    first_name: 'John',
+    last_name: 'Smith',
+    email: 'john.smith@me.com',
+    mobile_number: Faker::Number.leading_zero_number(digits: 11),
+    release_date: rand(1..2).even? ? Faker::Date.between(from: '2021-03-05', to: '2025-03-05') : '',
+    terms: true,
+    password: 'password'
+  )
+  user.skip_confirmation!
+  user.save!
 end
 
 Note.create!(
-  team_member_id: 1,
+  team_member_id: rand(1..TeamMember.count),
   visible_to_user: true,
-  user_id: User.first.id,
+  user_id: rand(1..User.count),
   content: Faker::TvShows::TheExpanse.quote
 )
 
 if WellbeingMetric.count.zero?
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Housing',
     category: 'Environment'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Benefits / Money',
     category: 'Environment'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Food',
     category: 'Environment'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Physical Health',
     category: 'Health'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Mental Health',
     category: 'Health'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Emotional Health',
     category: 'Health'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Behaviour',
     category: 'Personal'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Addiction',
     category: 'Health'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Relationships',
     category: 'Personal'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Sense of Community',
     category: 'Personal'
   )
   WellbeingMetric.create!(
-    team_member_id: 1,
+    team_member_id: rand(1..TeamMember.count),
     name: 'Employment/Education/Training',
     category: 'Personal'
   )
@@ -159,14 +171,16 @@ if WellbeingAssessment.count.zero?
     wba_count += 1
     puts("Creating Wellbeing Asessment #{wba_count}")
 
-    WellbeingAssessment.create!(
-      user_id: rand(1..10)
+    wellbeing_assessment = WellbeingAssessment.create!(
+      user_id: rand(1..User.count)
     )
 
-    WellbeingMetric.count.times do |wellbeing_metric_id|
+    wellbeing_assessment.update!(team_member_id: rand(1..TeamMember.count)) if (wba_count % 5).zero?
+
+    WellbeingMetric.all.each do |wellbeing_metric|
       WbaScore.create!(
-        wellbeing_assessment_id: wba_count,
-        wellbeing_metric_id: wellbeing_metric_id + 1,
+        wellbeing_assessment: wellbeing_assessment,
+        wellbeing_metric: wellbeing_metric,
         value: rand(1..10)
       )
     end
@@ -175,47 +189,41 @@ end
 
 CrisisType.create!(
   category: 'Self Harm',
-  team_member_id: 1
+  team_member_id: rand(1..TeamMember.count)
 )
 
 CrisisType.create!(
   category: 'Harming Others',
-  team_member_id: 1
+  team_member_id: rand(1..TeamMember.count)
 )
 
 CrisisType.create!(
   category: 'Suicide',
-  team_member_id: 1
+  team_member_id: rand(1..TeamMember.count)
 )
 
 CrisisType.create!(
   category: 'Overdose',
-  team_member_id: 1
+  team_member_id: rand(1..TeamMember.count)
 )
 
 CrisisType.create!(
   category: 'Domestic Violence',
-  team_member_id: 1
+  team_member_id: rand(1..TeamMember.count)
 )
 
 10.times do
+  crisis_event = CrisisEvent.create!(
+    additional_info: Faker::Hipster.sentences(number: 1)[0],
+    user_id: rand(1..User.count),
+    crisis_type_id: rand(1..CrisisType.count)
+  )
 
-  is_closed = [true, false].sample
+  next unless [true, false].sample
 
-  if is_closed
-    CrisisEvent.create!(
-      additional_info: Faker::Hipster.sentences(number: 1)[0],
-      closed: true,
-      closed_at: Faker::Date.between(from: '2020-03-05', to: '2021-03-05'),
-      closed_by: TeamMember.first,
-      user_id: rand(1..10),
-      crisis_type_id: rand(1..5)
-    )
-  else
-    CrisisEvent.create!(
-      additional_info: Faker::Hipster.sentences(number: 1)[0],
-      user_id: rand(1..10),
-      crisis_type_id: rand(1..5)
-    )
-  end
+  crisis_event.update!(
+    closed: true,
+    closed_at: Faker::Date.between(from: '2020-03-05', to: '2021-03-05'),
+    closed_by: TeamMember.find(rand(1..TeamMember.count))
+  )
 end
