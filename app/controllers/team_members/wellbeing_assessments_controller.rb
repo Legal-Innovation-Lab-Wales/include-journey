14c83cf8853cb55c8c11922825b56462f8b486ba
Ieuan Skinner <ieuanskinner@gmail.com>
2021-03-25 10:44:54 +0000

    IJ-117 Added a new route for a paginated team members wellbeing assessments view.

--- app/controllers/team_members/wellbeing_assessments_controller.rb
+++ app/controllers/team_members/wellbeing_assessments_controller.rb
@@ -1,6 +1,6 @@
 module TeamMembers
   # app/controllers/team_members/wellbeing_assessments_controller.rb
-  class WellbeingAssessmentsController < TeamMembersApplicationController
+  class WellbeingAssessmentsController < PaginationController
     before_action :wellbeing_assessment, only: :show
 
     # GET /wellbeing_assessment/:id
@@ -15,6 +15,22 @@ module TeamMembers
       team_member_name = @wellbeing_assessment.team_member.full_name
 
       "WBA for #{user_name} created by #{team_member_name} clicked!"
+    end
+
+    def resources
+      @admin = params[:team_member_id].present? && current_team_member.admin?
+
+      team_member
+
+      @resources = @team_member.wellbeing_assessments.includes(:user, :wba_scores).order(:created_at)
+    end
+
+    def team_member
+      @team_member = current_team_member
+
+      return unless @admin
+
+      @team_member = TeamMember.includes(:wellbeing_assessments).find(params[:team_member_id])
     end
 
     def wellbeing_assessment
