cb299991e5b57b51442d20e0879e7e49e87cf948
AlexW555 <871807@swansea.ac.uk>
2021-03-31 11:03:02 +0100

    IJ-122 Basic Styling and Form for create new appointment

--- app/controllers/users/appointments_controller.rb
+++ app/controllers/users/appointments_controller.rb
@@ -1,5 +1,5 @@
 module Users
-
+  # app/controllers/users/appointments_controller.rb
   class AppointmentsController < UsersApplicationController
 
     # GET /appointments/:id
@@ -9,6 +9,21 @@ module Users
 
     def index
       render 'users/appointments/appointments'
+    end
+
+    # POST /appointments
+    def create
+      if (@appointment = current_user.appointment.create!)
+        redirect_to new_journal_entry_permission_path(@appointment), success: 'New Appointment entry added'
+      else
+        redirect_to authenticated_user_root_path, error: "Appointment entry could not be created: #{@appointment.errors}"
+      end
+    end
+
+    private
+
+    def appointment
+      @appointment = Appointment.new
     end
   end
 end
