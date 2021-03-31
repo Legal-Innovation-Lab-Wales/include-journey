c60013074f0b3c15c15727f25af865f9e51befb6
AlexW555 <871807@swansea.ac.uk>
2021-03-31 13:33:30 +0100

    IJ-122 Rendering Appointments for a current_user

--- app/controllers/users/appointments_controller.rb
+++ app/controllers/users/appointments_controller.rb
@@ -1,6 +1,7 @@
 module Users
   # app/controllers/users/appointments_controller.rb
   class AppointmentsController < UsersApplicationController
+    before_action :appointments, only: :index
 
     # GET /appointments/:id
     def show
@@ -8,12 +9,12 @@ module Users
     end
 
     def index
-      render 'users/appointments/appointments'
+      render 'index'
     end
 
     # POST /appointments
     def create
-      if (@appointment = current_user.appointment.create!)
+      if (@appointment = current_user.appointments.create())
         redirect_to new_journal_entry_permission_path(@appointment), success: 'New Appointment entry added'
       else
         redirect_to authenticated_user_root_path, error: "Appointment entry could not be created: #{@appointment.errors}"
@@ -24,6 +25,10 @@ module Users
 
     def appointment
       @appointment = Appointment.new
+    end
+
+    def appointments
+      @appointments = current_user.appointments
     end
   end
 end
--- db/migrate/20210326120339_create_appointments.rb
+++ db/migrate/20210326120339_create_appointments.rb
@@ -2,7 +2,7 @@ class CreateAppointments < ActiveRecord::Migration[6.1]
   def change
     create_table :appointments do |t|
       t.belongs_to :user, null: false, foreign_key: true
-      t.datetime :when_datetime
+      t.datetime :when_datetime #TODO
       t.string :who_with
       t.string :where
       t.string :what
