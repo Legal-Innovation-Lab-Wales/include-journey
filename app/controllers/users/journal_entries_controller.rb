98465ee491bf32b1b030aebb447d7376724b564f
Ieuan Skinner <ieuanskinner@gmail.com>
2021-04-09 12:22:37 +0100

    User scope pagination controller has been refactored.

--- app/controllers/users/journal_entries_controller.rb
+++ app/controllers/users/journal_entries_controller.rb
@@ -1,11 +1,17 @@
 module Users
   # app/controllers/users/journal_entries_controller.rb
   class JournalEntriesController < PaginationController
-    before_action :journal_entry, only: :new
-    before_action :journal_entry_params, only: :create
+
+    # GET /journal_entries
+    def index
+      @resources_per_page = 3
+      super
+    end
 
     # GET /journal_entries/new
     def new
+      @journal_entry = JournalEntry.new
+
       render 'new'
     end
 
@@ -25,28 +31,18 @@ module Users
 
     private
 
-    def journal_entry
-      @journal_entry = JournalEntry.new
-    end
-
     def journal_entry_params
       params.require(:journal_entry).permit(:entry, :feeling)
     end
 
-    def journal_entry_search
-      'lower(entry) similar to lower(:query)'
-    end
-
-    def multiple
-      @multiple = 3
-    end
-
     def resources
-      @resources = if @query.present?
-                     current_user.journal_entries.where(journal_entry_search, wildcard_query).order(created_at: :desc)
-                   else
-                     current_user.journal_entries.order(created_at: :desc)
-                   end
+      @resources =
+        if @query.present?
+          current_user.journal_entries.where('lower(entry) similar to lower(:query)', wildcard_query)
+                      .order(created_at: :desc)
+        else
+          current_user.journal_entries.order(created_at: :desc)
+        end
     end
   end
 end
--- app/controllers/users/pagination_controller.rb
+++ app/controllers/users/pagination_controller.rb
@@ -1,62 +1,41 @@
 module Users
   # app/controllers/users/pagination_controller.rb
   class PaginationController < UsersApplicationController
-    before_action :query_params, :page, :query, :multiple, :limit, :offset, :resources,
-                  :count, :last_page, :limit_resources, :redirect, only: :index
-
+    # rubocop:disable Metrics/AbcSize
     def index
-      render 'index'
+      @page = query_params[:page].to_i < 1 ? 1 : query_params[:page].to_i
+      @query = query_params[:query]
+      @resources_per_page ||= 5
+      @resources = resources
+      @count = @resources.count
+      @last_page = offset + limit >= @count
+      @final_page = (@count.to_f / limit).ceil
+      @resources = @resources.offset(offset).limit(limit)
+      @resources.present? ? render('index') : redirect
     end
 
     protected
 
-    def count
-      @count = @resources.count
-    end
-
     def resources
       raise 'Subclass has not overridden resources function'
-    end
-
-    def last_page
-      @last_page = @offset + @limit >= @count
-      @final_page = (@count.to_f / @limit).ceil
     end
 
     def limit
       if query_params[:limit].present?
         limit = query_params[:limit].to_i
 
-        if limit.positive? && limit <= @multiple * 10
-          @limit = limit
+        if limit.positive? && limit <= @resources_per_page * 10
+          limit
         else
           redirect_back(fallback_location: authenticated_user_root_path, alert: 'Invalid Limit')
         end
       else
-        @limit = @multiple
+        @resources_per_page
       end
     end
 
-    def multiple
-      @multiple = 5
-    end
-
     def offset
-      @offset = (@page - 1) * @limit
-    end
-
-    def page
-      @page = params[:page].to_i
-
-      @page = 1 if @page < 1
-    end
-
-    def limit_resources
-      @resources = @resources.offset(@offset).limit(@limit).order(created_at: :desc)
-    end
-
-    def query
-      @query = query_params[:query]
+      (@page - 1) * limit
     end
 
     def query_params
@@ -64,13 +43,7 @@ module Users
     end
 
     def redirect
-      return if @resources.present?
-
-      redirect_back(fallback_location: authenticated_user_root_path, alert: 'None found')
-    end
-
-    def user_search
-      'lower(users.first_name) similar to lower(:query) or lower(users.last_name) similar to lower(:query)'
+      redirect_back(fallback_location: authenticated_user_root_path, alert: 'No Results Found')
     end
 
     def wildcard_query
