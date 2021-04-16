6084a2710e7fb2b768956d3bcaec06d16d0cec88
Ieuan Skinner <ieuanskinner@gmail.com>
2021-04-16 10:27:03 +0100

    IJ-136 - Adding blank actions for next steps.

--- app/controllers/team_members/notes_controller.rb
+++ app/controllers/team_members/notes_controller.rb
@@ -11,6 +11,21 @@ module TeamMembers
       redirect_to user_path(@user), notice: @note ? 'Note added!' : "The note couldn't be added. Please try again."
     end
 
+    # GET /notes/:id/edit
+    def edit
+      # render 'edit'
+    end
+
+    # POST /notes/:id/update
+    def update
+      # Do not change existing record, create new record and set replaced_by to the new record.
+    end
+
+    # GET /notes/:id
+    def show
+      # Show all notes in the edit-chain.
+    end
+
     private
 
     def user
