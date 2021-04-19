9d0bb89ddb8dec51824ac3c323b5cfcad3f50e4d
phil-6 <github@purpleriver.dev>
2021-04-19 09:27:04 +0100

    fix typo in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module IncludeJourney
     config.assets.version = '1.0'
 
     config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml, s')
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
       if File.exist?(env_file)
         YAML.safe_load(File.open(env_file)).each do |key, value|
           ENV[key.to_s] = value
