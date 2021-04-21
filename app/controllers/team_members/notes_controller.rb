9af57b88dc0d2216cf2ccd052063ed6f00239261
gda <garethdandrews@gmail.com>
2021-04-21 14:13:39 +0100

    Pass note ID to update modal/action. Added error handling for user and note in notes controller. Implemented update action.

--- app/controllers/team_members/notes_controller.rb
+++ app/controllers/team_members/notes_controller.rb
@@ -1,7 +1,8 @@
 module TeamMembers
   # app/controllers/team_members/notes_controller.rb
   class NotesController < TeamMembersApplicationController
-    before_action :note_params, :user, only: :create
+    before_action :note_params, :user, only: [:create, :update]
+    before_action :note, only: :update
 
     # POST /notes
     def create
@@ -21,9 +22,24 @@ module TeamMembers
       # render 'edit'
     end
 
-    # POST /notes/:id/update
+    # PUT /notes/:id/update
     def update
       # Do not change existing record, create new record and set replaced_by to the new record.
+      # check if there are any changes to the note
+      if @note[:content] == note_params[:content] || @note[:visible_to_user] == note_params[:visible_to_user]
+        redirect_to user_path(@user), notice: 'Nothing to update!'
+      else
+        unless (@new_note = current_team_member.notes.create!(content: note_params[:content],
+                                                              visible_to_user: note_params[:visible_to_user],
+                                                              user: @user))
+          redirect_to user_path(@user), flash: { error: 'Something went wrong. Please try again.' }
+        end
+        unless @note.update_column!(:replaced_by, @new_note)
+          # delete the new note if we fail to link it to the old note - otherwise, an old 'updated' note will be visible in the main notes list
+          @new_note.destroy!
+          redirect_to user_path(@user), flash: { error: 'Something went wrong. Please try again.' }
+        end
+      end
     end
 
     # DELETE
@@ -34,10 +50,18 @@ module TeamMembers
 
     def user
       @user = User.find(params[:user_id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_back(fallback_location: users_path, flash: { error: 'User not found' })
+    end
+
+    def note
+      @note = current_team_member.notes.find(note_params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_back(fallback_location: user_path, flash: { error: 'Note not found' })
     end
 
     def note_params
-      params.require(:note).permit(:content, :visible_to_user)
+      params.require(:note).permit(:id, :content, :visible_to_user)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -53,8 +53,8 @@ Rails.application.routes.draw do
         put 'increment', action: 'increment', on: :member, as: :increment
         put 'decrement', action: 'decrement', on: :member, as: :decrement
         put 'unpin', action: 'unpin', on: :member, as: :unpin
-        resources :notes, only: %i[create edit show destroy], on: :member
-        post 'update', controller: :notes, action: 'update', as: :update_note, on: :member
+        resources :notes, only: %i[create edit update show destroy], on: :member
+        # post 'update', controller: :notes, action: 'update', as: :update_note, on: :member
         resources :wellbeing_assessments, only: %i[new create index], on: :member
       end
 
