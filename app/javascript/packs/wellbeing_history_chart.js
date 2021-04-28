40cd008162e9968999806ef9bb121f391ba83d43
garethdandrews <garethdandrews@gmail.com>
2021-04-28 16:33:13 +0100

    updated color scale for wellbeing history chart

--- app/javascript/packs/wellbeing_history_chart.js
+++ app/javascript/packs/wellbeing_history_chart.js
@@ -1,16 +1,16 @@
 const chart_wrapper = document.querySelector('#wellbeing-history-chart-carousel .carousel-inner'),
     slide_buttons = document.querySelectorAll('#wellbeing-history-chart-carousel button'),
     scale = [
-        { description: "Abysmal", colour: "#E04444" },
-        { description: "Dreadful", colour: "#E64D52" },
-        { description: "Rubbish", colour: "#E86754" },
-        { description: "Bad", colour: "#EC8754" },
-        { description: "Mediocre", colour: "#F0A656" },
-        { description: "Fine", colour: "#DFC54C" },
-        { description: "Good", colour: "#BFCA48" },
-        { description: "Great", colour: "#9ECB46" },
-        { description: "Superb", colour: "#BFCB43" },
-        { description: "Perfect", colour: "#5DAD3A" }
+      { description: "Abysmal", colour: "#E04444" },
+      { description: "Dreadful", colour: "#e66043" },
+      { description: "Rubbish", colour: "#eb7945" },
+      { description: "Bad", colour: "#ee904b" },
+      { description: "Mediocre", colour: "#F0A656" },
+      { description: "Fine", colour: "#DFC54C" },
+      { description: "Good", colour: "#c1c041" },
+      { description: "Great", colour: "#a2ba3a" },
+      { description: "Superb", colour: "#82b438" },
+      { description: "Perfect", colour: "#5DAD3A" }
     ],
     create_chart = (history_chart, data) => {
         new Chart(history_chart.getContext('2d'), {
