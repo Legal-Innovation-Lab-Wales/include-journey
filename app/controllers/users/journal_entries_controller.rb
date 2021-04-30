09ece02b4c392a3327c6cef1475561b6446560ea
gda <garethdandrews@gmail.com>
2021-04-30 18:07:56 +0100

    Began implementing the overview for journal entries

--- app/controllers/users/journal_entries_controller.rb
+++ app/controllers/users/journal_entries_controller.rb
@@ -30,13 +30,18 @@ module Users
     end
 
     def resources
-      current_user.journal_entries.order(created_at: :desc)
+      @resources = current_user.journal_entries.order(created_at: :desc)
+      set_overview_stats
     end
 
     def search
       current_user.journal_entries
                   .where('lower(entry) similar to lower(:query)', wildcard_query)
                   .order(created_at: :desc)
+    end
+
+    def set_overview_stats
+      @entries_in_last_30_days = @resources.where('created_at >= ?', 30.days.ago).count
     end
   end
 end
