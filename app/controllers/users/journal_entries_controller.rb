4627cc8c616b892cf1126e23cb5a9a49791c3622
gda <garethdandrews@gmail.com>
2021-05-04 15:23:38 +0100

    Guard statement on subheading_stats

--- app/controllers/users/journal_entries_controller.rb
+++ app/controllers/users/journal_entries_controller.rb
@@ -36,6 +36,8 @@ module Users
     end
 
     def subheading_stats
+      return unless @resources.present?
+
       @entries_in_last_30_days = @resources.where('created_at >= ?', 30.days.ago).count
       @days_since_last_entry = (Time.now.to_date - @resources.first.created_at.to_date).to_i
     end
