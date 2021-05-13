6e0adf0cd20ac033b4d3eab87e2bcd6a9b74dc5a
gda <garethdandrews@gmail.com>
2021-05-13 10:51:07 +0100

    Updated model and migration to include view_count

--- app/models/journal_entry_view_log.rb
+++ app/models/journal_entry_view_log.rb
@@ -1,7 +1,15 @@
 # app/models/journal_entry_view_log.rb
 class JournalEntryViewLog < ApplicationRecord
-  belongs_to :journal_entry
+  after_initialize :increment_view_count
+
   belongs_to :team_member
+  belongs_to :journal_entry
 
   has_one :user, through: :journal_entry
+
+  validates_presence_of :team_member_id, :journal_entry_id
+
+  def increment_view_count
+    self.view_count += 1
+  end
 end
