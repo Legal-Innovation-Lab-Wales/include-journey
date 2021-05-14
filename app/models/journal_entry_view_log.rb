054df6dd5e31ff83ee0921df1b78fdacebbb1e3b
Ieuan Skinner <ieuanskinner@gmail.com>
2021-05-14 13:38:01 +0100

    IJ-173 - Added additional sort for the published at column and also added in the subheading stats.

--- app/models/journal_entry_view_log.rb
+++ app/models/journal_entry_view_log.rb
@@ -2,8 +2,10 @@
 class JournalEntryViewLog < ApplicationRecord
   belongs_to :team_member
   belongs_to :journal_entry
-
   has_one :user, through: :journal_entry
+
+  scope :viewed_in_last_week, -> { where('journal_entry_view_logs.updated_at >= ?', 1.week.ago) }
+  scope :viewed_in_last_month, -> { where('journal_entry_view_logs.updated_at >= ?', 1.month.ago) }
 
   validates_presence_of :team_member_id, :journal_entry_id
 
