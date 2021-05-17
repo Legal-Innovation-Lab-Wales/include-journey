8c4e1303001fa62d107c361cc59ae893bc2a78f3
Ieuan Skinner <ieuanskinner@gmail.com>
2021-05-17 13:05:42 +0100

    IJ-92 - More style updates to the pyramid view switch.

--- app/javascript/packs/pyramid_view.js
+++ app/javascript/packs/pyramid_view.js
@@ -1,4 +1,8 @@
 const pyramid_view_switch = document.querySelector('#pyramid-view-switch'),
+      pyramid_view_switch_text = pyramid_view_switch.querySelector('span'),
       pyramid_view = document.querySelector('#pyramid-view')
 
-pyramid_view_switch.addEventListener('change', () => pyramid_view.classList.toggle('flip'))
+pyramid_view_switch.addEventListener('click', () => {
+    pyramid_view.classList.toggle('flip')
+    pyramid_view_switch_text.innerText = `View ${pyramid_view.classList.contains('flip') ? 'Chart': 'Pyramid'}`
+})
