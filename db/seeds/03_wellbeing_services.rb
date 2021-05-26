f80051462336807f5df6586fc0c59c6712364583
Phil Reynolds <github@purpleriver.dev>
2021-05-26 09:59:08 +0100

    fix wellbeing services contact number display

--- db/seeds/03_wellbeing_services.rb
+++ db/seeds/03_wellbeing_services.rb
@@ -5,42 +5,42 @@ if WellbeingService.count.zero?
     name: 'Include UK',
     description: 'Help for ex-offenders',
     website: 'https://include-uk.com',
-    contact_number: '01792 814792'
+    contact_number: '01792814792'
   )
   WellbeingService.create!(
     team_member_id: rand(1..TeamMember.count),
     name: 'Samaritans',
     description: 'Support phone line',
     website: 'https://www.samaritans.org',
-    contact_number: '01792 814792'
+    contact_number: '01792814792'
   )
   WellbeingService.create!(
     team_member_id: rand(1..TeamMember.count),
     name: 'Mind',
     description: 'Mental health charity',
     website: 'https://www.mind.org.uk',
-    contact_number: '01792 814792'
+    contact_number: '01792814792'
   )
   WellbeingService.create!(
     team_member_id: rand(1..TeamMember.count),
     name: 'Shelter',
     description: 'Housing support',
     website: 'https://www.shelter.org.uk',
-    contact_number: '01792 814792'
+    contact_number: '01792814792'
   )
   WellbeingService.create!(
     team_member_id: rand(1..TeamMember.count),
     name: 'Citizens Advice',
     description: 'Government Services',
     website: 'https://citizensadvicesnpt.org.uk',
-    contact_number: '01792 814792'
+    contact_number: '01792814792'
   )
   WellbeingService.create!(
     team_member_id: rand(1..TeamMember.count),
     name: 'Food Banks',
     description: 'Trussell Trust Food Bank Search',
     website: 'https://www.trusselltrust.org/get-help/find-a-foodbank',
-    contact_number: '01792 814792'
+    contact_number: '01792814792'
   )
 
   puts "\tDuration: #{Time.now - @last_time}\tElapsed: #{Time.now - @start_time}"
