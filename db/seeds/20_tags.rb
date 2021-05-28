05cd4e993c06112585beaea040f5a18fb5db557d
AlexW555 <871807@swansea.ac.uk>
2021-05-28 11:45:29 +0100

    bring tags and user tags field up to date with new format

--- db/seeds/20_tags.rb
+++ db/seeds/20_tags.rb
@@ -1,4 +1,5 @@
 if Tag.count.zero?
+  print "Tags\t\t\tStart: #{Time.now - @start_time}"
   Tag.create!(tag: 'DV Victim', team_member_id: rand(1..TeamMember.count), created_at: rand(1..30).days.ago)
   Tag.create!(tag: 'DV Perpetrator', team_member_id: rand(1..TeamMember.count), created_at: rand(1..30).days.ago)
   Tag.create!(tag: 'Sexual Violence', team_member_id: rand(1..TeamMember.count), created_at: rand(1..30).days.ago)
@@ -7,6 +8,6 @@ if Tag.count.zero?
   Tag.create!(tag: 'Mental Health', team_member_id: rand(1..TeamMember.count), created_at: rand(1..30).days.ago)
   Tag.create!(tag: "Women's Pathfinder Project", team_member_id: rand(1..TeamMember.count), created_at: rand(1..30).days.ago)
 
-  puts "Tags\t\t\tTime: #{Time.now - @last_time}\tElapsed: #{Time.now - @start_time}"
+  puts "\tDuration: #{Time.now - @last_time}\tElapsed: #{Time.now - @start_time}"
   @last_time = Time.now
 end
