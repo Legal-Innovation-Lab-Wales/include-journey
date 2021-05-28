46455af571c98228e0c73c2d7107bef97ea98b74
AlexW555 <871807@swansea.ac.uk>
2021-05-28 14:10:02 +0100

    IJ-182 Implemented score values, allowing editing of naming metrics

--- app/controllers/team_members/wellbeing_score_values_controller.rb
+++ app/controllers/team_members/wellbeing_score_values_controller.rb
@@ -3,18 +3,24 @@ module TeamMembers
   class WellbeingScoreValuesController < TeamMembersApplicationController
     # GET /wellbeing_score_values
     def index
-      @score_values = WellbeingScoreValue.all
+      @score_values = WellbeingScoreValue.all.order(:value)
       render 'index'
     end
 
     # PUT /wellbeing_score_values/:id
     def update
+      @score_values = WellbeingScoreValue.find(params[:id])
+      if @score_values.update(wellbeing_score_params)
+        redirect_to wellbeing_score_values_path, flash: { success: "Wellbeing Score values updated"}
+      else
+        redirect_to wellbeing_score_values_path, flash: { error: "Wellbeing score values not updated. Please try again"}
+      end
     end
 
     private
 
-    def wellbeing_metric_params
-      # params.require()
+    def wellbeing_score_params
+      params.require(:wellbeing_score_value).permit(:name)
     end
   end
 end
