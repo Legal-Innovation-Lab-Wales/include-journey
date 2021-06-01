ec4ffc9970d638883daee89126494dd337ebbc42
gda <garethdandrews@gmail.com>
2021-06-01 11:39:30 +0100

    File comments

--- app/helpers/mailer_helper.rb
+++ app/helpers/mailer_helper.rb
@@ -1,3 +1,4 @@
+# app/helpers/mailer_help.rb
 module MailerHelper
   def email_image_tag(image, **options)
     attachments.inline[image] = File.read(Rails.root.join("app/assets/images/mailer/#{image}"))
