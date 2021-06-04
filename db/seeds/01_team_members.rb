f5791559f9c4df427fa248392005e4a6828396cc
garethdandrews <garethdandrews@gmail.com>
2021-06-04 10:27:52 +0100

    Styling

--- db/seeds/01_team_members.rb
+++ db/seeds/01_team_members.rb
@@ -1,67 +1,67 @@
 print "Team Members\t\tStart: #{Time.now - @start_time}"
-unless TeamMember.find_by_email('philr@purpleriver.dev').present?
-  team_member = TeamMember.new(
-    first_name: 'Phil',
-    last_name: 'Reynolds',
-    email: 'philr@purpleriver.dev',
-    mobile_number: '07888777666',
-    admin: true,
-    approved: true,
-    terms: true,
-    suspended: false,
-    password: 'password'
-  )
-  team_member.skip_confirmation!
-  team_member.save!
-end
+# unless TeamMember.find_by_email('philr@purpleriver.dev').present?
+#   team_member = TeamMember.new(
+#     first_name: 'Phil',
+#     last_name: 'Reynolds',
+#     email: 'philr@purpleriver.dev',
+#     mobile_number: '07888777666',
+#     admin: true,
+#     approved: true,
+#     terms: true,
+#     suspended: false,
+#     password: 'password'
+#   )
+#   team_member.skip_confirmation!
+#   team_member.save!
+# end
 
-unless TeamMember.find_by_email('a.j.wing@swansea.ac.uk').present?
-  team_member = TeamMember.new(
-    first_name: 'Alex',
-    last_name: 'Wing',
-    email: 'a.j.wing@swansea.ac.uk',
-    mobile_number: '07777666555',
-    admin: true,
-    approved: true,
-    terms: true,
-    suspended: false,
-    password: 'password'
-  )
-  team_member.skip_confirmation!
-  team_member.save!
-end
+# unless TeamMember.find_by_email('a.j.wing@swansea.ac.uk').present?
+#   team_member = TeamMember.new(
+#     first_name: 'Alex',
+#     last_name: 'Wing',
+#     email: 'a.j.wing@swansea.ac.uk',
+#     mobile_number: '07777666555',
+#     admin: true,
+#     approved: true,
+#     terms: true,
+#     suspended: false,
+#     password: 'password'
+#   )
+#   team_member.skip_confirmation!
+#   team_member.save!
+# end
 
-unless TeamMember.find_by_email('ieuan.skinner@swansea.ac.uk').present?
-  team_member = TeamMember.new(
-    first_name: 'Ieuan',
-    last_name: 'Skinner',
-    email: 'ieuan.skinner@swansea.ac.uk',
-    mobile_number: '07666555444',
-    admin: true,
-    approved: true,
-    terms: true,
-    suspended: false,
-    password: 'password'
-  )
-  team_member.skip_confirmation!
-  team_member.save!
-end
+# unless TeamMember.find_by_email('ieuan.skinner@swansea.ac.uk').present?
+#   team_member = TeamMember.new(
+#     first_name: 'Ieuan',
+#     last_name: 'Skinner',
+#     email: 'ieuan.skinner@swansea.ac.uk',
+#     mobile_number: '07666555444',
+#     admin: true,
+#     approved: true,
+#     terms: true,
+#     suspended: false,
+#     password: 'password'
+#   )
+#   team_member.skip_confirmation!
+#   team_member.save!
+# end
 
-unless TeamMember.find_by_email('benmharrison@me.com').present?
-  team_member = TeamMember.new(
-    first_name: 'Ben',
-    last_name: 'Harrison',
-    email: 'benmharrison@me.com',
-    mobile_number: '07555444333',
-    admin: true,
-    approved: true,
-    terms: true,
-    suspended: false,
-    password: 'password'
-  )
-  team_member.skip_confirmation!
-  team_member.save!
-end
+# unless TeamMember.find_by_email('benmharrison@me.com').present?
+#   team_member = TeamMember.new(
+#     first_name: 'Ben',
+#     last_name: 'Harrison',
+#     email: 'benmharrison@me.com',
+#     mobile_number: '07555444333',
+#     admin: true,
+#     approved: true,
+#     terms: true,
+#     suspended: false,
+#     password: 'password'
+#   )
+#   team_member.skip_confirmation!
+#   team_member.save!
+# end
 
 unless TeamMember.find_by_email('g.d.andrews@swansea.ac.uk').present?
   team_member = TeamMember.new(
