6b9e5e77623bcab48e22cad4ee7d26bea7366ef2
Ieuan Skinner <ieuanskinner@gmail.com>
2021-07-30 11:32:25 +0100

    IJ-191 - Index view for team member showing all the upcoming daily affirmations is now complete and linked to by the dashboard and navbar.

--- app/controllers/team_members/affirmations_controller.rb
+++ app/controllers/team_members/affirmations_controller.rb
@@ -3,6 +3,10 @@ module TeamMembers
   class AffirmationsController < TeamMembersApplicationController
     # GET /affirmations
     def index
+      @affirmations = Affirmation.includes(:team_member)
+                                 .where('scheduled_date >= ?', Date.today)
+                                 .order(scheduled_date: :asc)
+
       render 'index'
     end
 
--- app/models/affirmation.rb
+++ app/models/affirmation.rb
@@ -1,5 +1,10 @@
+# app/models/affirmation.rb
 class Affirmation < ApplicationRecord
   belongs_to :team_member
 
   validates_presence_of :text, :team_member_id
+
+  def date
+    scheduled_date.strftime('%d/%m/%Y')
+  end
 end
