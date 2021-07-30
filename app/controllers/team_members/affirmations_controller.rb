5a0de8d0aa9535cc08f572d054d344ad6f43013d
Ieuan Skinner <ieuanskinner@gmail.com>
2021-07-30 13:22:01 +0100

    IJ-191 - Daily Affirmations can now be edited and destroyed by team members.

--- app/controllers/team_members/affirmations_controller.rb
+++ app/controllers/team_members/affirmations_controller.rb
@@ -1,6 +1,9 @@
 module TeamMembers
   # app/controllers/team_members/affirmations_controller.rb
   class AffirmationsController < TeamMembersApplicationController
+    before_action :affirmation, except: %i[index create]
+    before_action :unique_check, only: :update
+
     # GET /affirmations
     def index
       @affirmations = Affirmation.includes(:team_member)
@@ -15,14 +18,49 @@ module TeamMembers
 
     end
 
-    # PUT /affirmations
-    def update
-
+    # GET /affirmations/:id/edit
+    def edit
+      render 'edit'
     end
 
-    # DELETE /affirmations
-    def destroy
+    # PUT /affirmations/:id
+    def update
+      @affirmation.update(
+        text: affirmation_params[:text],
+        scheduled_date: affirmation_params[:scheduled_date],
+        team_member: current_team_member
+      )
 
+      redirect_to affirmations_path, flash: { success: 'Daily Affirmation was successfully updated' }
+    end
+
+    # DELETE /affirmations/:id
+    def destroy
+      @affirmation.destroy!
+
+      redirect_to affirmations_path, flash: { success: 'Daily Affirmation was successfully removed' }
+    end
+
+    private
+
+    def affirmation
+      @affirmation = Affirmation.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_back(fallback_location: affirmations_path,
+                    flash: { error: 'Daily Affirmation not found' })
+    end
+
+    def affirmation_params
+      params.require(:affirmation).permit(:text, :scheduled_date)
+    end
+
+    def unique_check
+      date = affirmation_params[:scheduled_date]
+
+      return unless Affirmation.where('id != ? and scheduled_date = ?', @affirmation.id, date).present?
+
+      redirect_back(fallback_location: affirmation_path(@affirmation),
+                    flash: { error: "A Daily Affirmation already exists for #{Date.parse(date).strftime('%d/%m/%Y')}" })
     end
   end
 end
