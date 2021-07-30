af8db6cde9328b70bdb8d06eb9018df6dc7c98e9
Ieuan Skinner <ieuanskinner@gmail.com>
2021-07-30 14:05:28 +0100

    IJ-191 - Team members can now add new affirmations.

--- app/controllers/team_members/affirmations_controller.rb
+++ app/controllers/team_members/affirmations_controller.rb
@@ -1,8 +1,11 @@
 module TeamMembers
   # app/controllers/team_members/affirmations_controller.rb
   class AffirmationsController < TeamMembersApplicationController
-    before_action :affirmation, except: %i[index create]
-    before_action :unique_check, only: :update
+    before_action :affirmation, except: %i[index new create]
+    before_action :update_unique_check, only: :update
+    before_action :create_unique_check, only: :create
+
+    after_action :clear_session_data, only: :new
 
     # GET /affirmations
     def index
@@ -13,9 +16,26 @@ module TeamMembers
       render 'index'
     end
 
+    # GET /affirmations/new
+    def new
+      text = session[:affirmation_text].present? ? session[:affirmation_text] : ''
+      latest = Affirmation.order(scheduled_date: :desc).first
+      date = latest.present? && latest.scheduled_date > Date.today ? latest.scheduled_date + 1.days : Date.today
+
+      @affirmation = Affirmation.new(text: text, scheduled_date: date)
+
+      render 'new'
+    end
+
     # POST /affirmations
     def create
+      @affirmation = Affirmation.create!(
+        text: affirmation_params[:text],
+        scheduled_date: affirmation_params[:scheduled_date],
+        team_member: current_team_member
+      )
 
+      success('created')
     end
 
     # GET /affirmations/:id/edit
@@ -31,14 +51,14 @@ module TeamMembers
         team_member: current_team_member
       )
 
-      redirect_to affirmations_path, flash: { success: 'Daily Affirmation was successfully updated' }
+      success('updated')
     end
 
     # DELETE /affirmations/:id
     def destroy
       @affirmation.destroy!
 
-      redirect_to affirmations_path, flash: { success: 'Daily Affirmation was successfully removed' }
+      success('removed')
     end
 
     private
@@ -54,13 +74,34 @@ module TeamMembers
       params.require(:affirmation).permit(:text, :scheduled_date)
     end
 
-    def unique_check
+    def success(action)
+      redirect_to affirmations_path, flash: { success: "Daily Affirmation was successfully #{action}" }
+    end
+
+    def update_unique_check
       date = affirmation_params[:scheduled_date]
 
       return unless Affirmation.where('id != ? and scheduled_date = ?', @affirmation.id, date).present?
 
-      redirect_back(fallback_location: affirmation_path(@affirmation),
-                    flash: { error: "A Daily Affirmation already exists for #{Date.parse(date).strftime('%d/%m/%Y')}" })
+      redirect_back(fallback_location: affirmation_path(@affirmation), flash: error('update'))
+    end
+
+    def create_unique_check
+      return unless Affirmation.where('scheduled_date = ?', affirmation_params[:scheduled_date]).present?
+
+      session[:affirmation_text] = affirmation_params[:text]
+
+      redirect_back(fallback_location: new_affirmation_path, flash: error('create'))
+    end
+
+    def error(action)
+      date = Date.parse(affirmation_params[:scheduled_date]).strftime('%d/%m/%Y')
+
+      { error: "Daily Affirmation not #{action}d: an affirmation already exists for #{date}" }
+    end
+
+    def clear_session_data
+      session.delete(:affirmation_text)
     end
   end
 end
