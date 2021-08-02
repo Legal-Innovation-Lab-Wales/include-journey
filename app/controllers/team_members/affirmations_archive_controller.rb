183385ac4998261c074d99dbcd3ab7742e69d26b
Ieuan Skinner <ieuanskinner@gmail.com>
2021-08-02 09:31:50 +0100

    IJ-192 - Affirmations archive search now supports searching based on the affirmation text.

--- app/controllers/team_members/affirmations_archive_controller.rb
+++ app/controllers/team_members/affirmations_archive_controller.rb
@@ -11,16 +11,30 @@ module TeamMembers
     protected
 
     def resources
-      Affirmation.includes(:team_member).archived.order(sort)
+      Affirmation.includes(:team_member)
+                 .archived
+                 .order(sort)
     end
 
     def search
-      self.resources
+      Affirmation.includes(:team_member)
+                 .where(affirmations_archive_search, query_terms)
+                 .order(sort)
     end
 
     def sort
       @sort = 'scheduled_date'
       { "#{@sort}": @direction }
+    end
+
+    private
+
+    def query_terms
+      wildcard_query.merge({ today: Date.today })
+    end
+
+    def affirmations_archive_search
+      'scheduled_date < :today and lower(text) similar to lower(:query)'
     end
   end
 end
