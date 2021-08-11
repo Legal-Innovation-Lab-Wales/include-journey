ea43a24f5b27bca91b1ef41e96327d4884566c55
Ieuan Skinner <ieuanskinner@gmail.com>
2021-08-11 09:40:40 +0100

    IJ-185 - Comment section labels are now editable via the survey edit UI.

--- app/controllers/team_members/survey_comment_sections_controller.rb
+++ app/controllers/team_members/survey_comment_sections_controller.rb
@@ -9,16 +9,26 @@ module TeamMembers
       redirect_back(fallback_location: edit_survey_path(@survey), flash: { success: 'New Comment Section added' })
     end
 
-    # PUT /surveys/:survey_id/survey_sections/:section_id/survey_questions/:id
+    # PUT /surveys/:survey_id/survey_sections/:section_id/survey_comment_sections/:id
     def update
-      puts 'Update action called...'
+      @survey_comment_section.update!(comment_section_params)
+
+      respond_to do |format|
+        format.json { render json: @survey_comment_section.as_json, status: :ok }
+      end
     end
 
-    # DELETE /surveys/:survey_id/survey_sections/:section_id/survey_questions/:id
+    # DELETE /surveys/:survey_id/survey_sections/:section_id/survey_comment_sections/:id
     def destroy
       @survey_comment_section.destroy!
 
       redirect_back(fallback_location: edit_survey_path(@survey), flash: { success: 'Comment Section removed' })
+    end
+
+    private
+
+    def comment_section_params
+      params.require(:survey_comment_section).permit(:label)
     end
   end
 end
