ca2163fff4021655d7847b210513449ab44e65e6
Ieuan Skinner <ieuanskinner@gmail.com>
2021-08-11 11:26:38 +0100

    IJ-185 - Questions can now be dragged and dropped in the UI.

--- app/javascript/packs/survey_drag_drop.js
+++ app/javascript/packs/survey_drag_drop.js
@@ -1,6 +1,29 @@
-const draggable_rows = document.querySelectorAll('.row[draggable="true"]')
+const questions = document.querySelectorAll('.row.question'),
+    index = question => Array.from(question.parentNode.children).indexOf(question)
+    reset_border = question => question.classList.remove('drop-border-bottom', 'drop-border-top')
 
-draggable_rows.forEach(draggable_row => {
-    draggable_row.addEventListener('dragstart', () => draggable_row.style.opacity = 0.4)
-    draggable_row.addEventListener('dragend', () => draggable_row.style.opacity = 1)
+let drag_src_el = null
+
+questions.forEach(question => {
+    question.addEventListener('dragstart', e => {
+        question.style.opacity = 0.4
+        drag_src_el = question
+    })
+    question.addEventListener('dragend', () => question.style.opacity = 1)
+    question.addEventListener('dragenter', e => e.preventDefault())
+    question.addEventListener('dragover', e => {
+        e.preventDefault()
+        const drag_order = index(drag_src_el), drop_order = index(question)
+        if (drop_order !== drag_order)
+            question.classList.add(`drop-border-${drop_order > drag_order ? 'bottom' : 'top'}`)
+    })
+    question.addEventListener('dragleave', () => reset_border(question))
+    question.addEventListener('drop', e => {
+        e.preventDefault()
+        const drag_order = index(drag_src_el), drop_order = index(question)
+        if (drop_order !== drag_order) {
+            question.insertAdjacentElement(drop_order > drag_order ? 'afterend' : 'beforebegin', drag_src_el)
+            reset_border(question)
+        }
+    })
 })
