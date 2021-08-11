59f648b348298414174dd9e59af4c56fff49ff58
Ieuan Skinner <ieuanskinner@gmail.com>
2021-08-11 09:16:51 +0100

    IJ-185 - Cleaned up JS for the edit survey.

--- app/javascript/packs/edit_survey.js
+++ app/javascript/packs/edit_survey.js
@@ -2,30 +2,23 @@ const csrf_tokens = document.getElementsByName('csrf-token'),
     survey_url = `${location.origin}/${location.pathname.replace('/edit', '')}`,
     headers = {'Content-Type': 'application/json', 'X-CSRF-Token': csrf_tokens.length > 0 ? csrf_tokens[0].content : ''},
     survey_details = document.querySelector('form#survey'),
-    survey_fields = document.querySelectorAll('input[name^=survey]'),
-    survey_name = survey_details.querySelector('input[name="survey[name]"]'),
-    survey_start_date = survey_details.querySelector('input[name="survey[start_date]"]'),
-    survey_end_date = survey_details.querySelector('input[name="survey[end_date]"]'),
+    survey_fields = survey_details.querySelectorAll('input[name^=survey]'),
+    survey_field_regexp = new RegExp(/survey\[(\w+)\]/),
     section_headings = document.querySelectorAll('input[name^=section]'),
     update_survey_fields = () => {
-        fetch(survey_url, {
-            method: 'put',
-            headers: headers,
-            body: JSON.stringify({
-                survey: { name: survey_name.value, start_date: survey_start_date.value, end_date: survey_end_date.value }
-            })
-        })
+        const survey_data = [...survey_fields].map(field =>
+            [field.getAttribute('name').match(survey_field_regexp)[1], field.value]),
+            body = JSON.stringify({ survey: Object.fromEntries(new Map(survey_data)) })
+
+        fetch(survey_url, { method: 'put', headers: headers, body: body })
             .then(response => { if (!response.ok) throw 'Survey details could not be updated!' })
             .catch(error => alert(error))
     },
     update_survey_heading = e => {
-        const section_id = e.target.closest('.survey-section').dataset.id
+        const section_id = e.target.closest('.survey-section').dataset.id,
+            body = JSON.stringify({ survey_section: { heading: e.target.value } })
 
-        fetch(`${survey_url}/survey_sections/${section_id}`, {
-            method: 'put',
-            headers: headers,
-            body: JSON.stringify({ survey_section: { heading: e.target.value } })
-        })
+        fetch(`${survey_url}/survey_sections/${section_id}`, { method: 'put', headers: headers, body: body })
             .then(response => { if (!response.ok) throw 'Survey section label could not be updated!' })
             .catch(error => alert(error))
     }
