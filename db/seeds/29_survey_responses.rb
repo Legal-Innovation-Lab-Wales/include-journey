294772886cbb19b3698fe417f59850cf8d81abc4
Ieuan Skinner <ieuanskinner@gmail.com>
2021-08-12 09:45:09 +0100

    IJ-185 - Survey responses can be created for every user from the seed if a config property is set.

---
+++ db/seeds/29_survey_responses.rb
@@ -0,0 +1,29 @@
+if Config::SURVEY_RESPONSES
+  print "#{pretty_print_name('Survey Responses')}\tStart: #{pretty_print(Time.now - @start_time)}"
+
+  survey = Survey.includes(:survey_sections).first
+  survey_sections = survey.survey_sections.includes(:survey_questions, :survey_comment_sections)
+
+  User.all.each do |user|
+    response = SurveyResponse.create!(user: user, survey: survey)
+
+    survey_sections.each do |survey_section|
+      survey_section.survey_questions.each do |question|
+        SurveyAnswer.create!(answer: rand(1..6), survey_question: question, survey_response: response)
+      end
+
+      survey_section.survey_comment_sections.each do |comment_section|
+        SurveyComment.create!(
+          text: Faker::Movies::HitchhikersGuideToTheGalaxy.quote,
+          survey_comment_section: comment_section,
+          survey_response: response
+        )
+      end
+    end
+
+    response.update!(submitted_at: DateTime.now)
+  end
+
+  puts "\tDuration: #{pretty_print(Time.now - @last_time)}   Elapsed: #{pretty_print(Time.now - @start_time)}"
+  @last_time = Time.now
+end
