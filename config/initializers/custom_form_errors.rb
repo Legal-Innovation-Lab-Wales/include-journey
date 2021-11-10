ff378cf17eb6aeb4d7c1f9630b2ea07ea2228212
garethdandrews <garethdandrews@gmail.com>
2021-11-10 15:41:53 +0000

    Added custom form error stylings, removed error messages partial from forms

---
+++ config/initializers/custom_form_errors.rb
@@ -0,0 +1,31 @@
+ActionView::Base.field_error_proc = proc do |html_tag, instance|
+  fragment = Nokogiri::HTML.fragment(html_tag)
+  field = fragment.at('input,select,textarea')
+  error_messages = instance.object.errors.messages
+
+  html = nil
+  if field
+    field['class'] = "#{field['class']} invalid"
+
+    value = field.attributes['name']
+                 .value
+                 .match(/\[(.*?)\]/)[0]
+                 .delete('[]')
+                 .gsub('_', ' ')
+
+    error_message = error_messages[value.to_sym][0]
+    error_message = 'is invalid' unless error_message.present?
+
+    html = <<HTML
+#{fragment.to_s} 
+<span class=\"error-message\">
+#{value.capitalize} #{error_message}
+</span>
+HTML
+    html
+  else
+    html = html_tag
+  end
+
+  html.html_safe
+end
