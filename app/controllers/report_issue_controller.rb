fa0b8ae5e12a5cd8a72e33969c7c22ce20a3bb3a
Tobias Sheer Hardwick <zaenilith@gmail.com>
2022-08-16 11:07:52 +0100

    Change report emailer to pass params

--- app/controllers/report_issue_controller.rb
+++ app/controllers/report_issue_controller.rb
@@ -1,7 +1,9 @@
 # app/controllers/report_issue_controller.rb
 class ReportIssueController < ApplicationController
   def send_report
-    ReportsMailer.with(params[:report]).report_issue_email.deliver_now
-    redirect_back(fallback_location: root_path, flash: { success: "Report sent!" })
+    ReportsMailer.with(params[:report]).report_issue_email(
+      current_team_member ? current_team_member.email : current_user.email
+    ).deliver_now
+    redirect_back(fallback_location: root_path, flash: { success: 'Report sent!' })
   end
 end
--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -1,9 +1,9 @@
 # app/mailers/reports_mailer.rb
 class ReportsMailer < ApplicationMailer
-  def report_issue_email
+  def report_issue_email(email)
     @type = params[:type]
     @body = params[:body]
-    @email = current_team_member ? current_team_member.email : current_user.email
+    @email = email
     mail(to: ENV['REPORT_EMAIL'], from: @email, subject: "Issue Reported: #{@type}")
   end
 end
