9f790f2275b6d3a86e6c57a545a9c13dadf7d811
Tobias Sheer Hardwick <zaenilith@gmail.com>
2022-08-16 11:24:01 +0100

    CHanged emails to have newline

--- app/mailers/reports_mailer.rb
+++ app/mailers/reports_mailer.rb
@@ -2,7 +2,7 @@
 class ReportsMailer < ApplicationMailer
   def report_issue_email(email)
     @type = params[:type]
-    @body = "#{email} <br> #{params[:body]}"
+    @body = "#{email} \n #{params[:body]}"
     mail(to: ENV['REPORT_EMAIL'], subject: "Issue Reported: #{@type}")
   end
 end
