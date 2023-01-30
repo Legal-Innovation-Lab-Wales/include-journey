255f6399834f8fb60df204e0a048744c713be04a
Tobias Sheer Hardwick <zaenilith@gmail.com>
2023-01-30 07:37:00 +0000

    Contact logs UI improvements

--- app/controllers/team_members/contact_logs_controller.rb
+++ app/controllers/team_members/contact_logs_controller.rb
@@ -6,17 +6,20 @@ module TeamMembers
     include Pagination
     before_action :validate_dates, only: :create
 
-    # GET /contact_logs/upcoming
+    # GET /contact_logs/recent
     def recent
-      @contact_logs = current_team_member.contact_logs.recent.order(start: :asc)
+      @contact_logs = current_team_member.contact_logs.recent.order(start: :desc)
+      @count_in_last_week = @contact_logs.last_week.size
+      @count_in_last_month = @contact_logs.last_month.size
+
       render 'recent'
     end
 
     # POST /contact_logs
     def create
       @contact_log = ContactLog.new(
-        user: contact_log_params[:user],
-        contact_type: contact_log_params[:contact_type],
+        user: User.where(id: contact_log_params[:user_id]).first,
+        contact_type: ContactType.where(id: contact_log_params[:contact_type_id]).first,
         notes: contact_log_params[:notes],
         start: contact_log_params[:start],
         end: contact_log_params[:end],
@@ -25,9 +28,11 @@ module TeamMembers
 
       if @contact_log.save
         session.delete(:contact_log_params)
-        redirect_to upcoming_contact_logs_path, flash: { success: 'Contact log created' }
+        redirect_to recent_contact_logs_path, flash: { success: 'Contact log created' }
       else
         add_breadcrumb('New Contact log', nil, 'fas fa-plus circle')
+        form_resources
+
         render 'new'
       end
     end
@@ -36,6 +41,7 @@ module TeamMembers
     def new
       add_breadcrumb('New Contact log', nil, 'fas fa-plus-circle')
       @contact_log = ContactLog.new(session[:contact_log_params])
+      form_resources
 
       render 'new'
     end
@@ -43,13 +49,16 @@ module TeamMembers
     # GET /contact_logs/:id/edit
     def edit
       add_breadcrumb('Edit Contact Log', nil, 'fas fa-edit')
+      form_resources
+
       render 'edit'
     end
 
     # PUT /contact_logs/:id
     def update
-      if @contact_log.update!(contact_log_params)
-        redirect_to upcoming_contact_logs_path, flash: { success: 'Contact log updated' }
+      if @contact_log.update(contact_log_params)
+        @contact_log.save
+        redirect_to recent_contact_logs_path, flash: { success: 'Contact log updated' }
       else
         redirect_to edit_contact_log_path(@contact_log),
                     flash: { error: 'Contact log could not be updated. Please try again.' }
@@ -59,9 +68,9 @@ module TeamMembers
     # DELETE /contact_logs/:id
     def destroy
       if @contact_log.destroy!
-        redirect_to upcoming_contact_logs_path, flash: { success: 'Contact log deleted' }
+        redirect_to recent_contact_logs_path, flash: { success: 'Contact log deleted' }
       else
-        redirect_to upcoming_contact_logs_path(@contact_log),
+        redirect_to recent_contact_logs_path(@contact_log),
                     flash: { error: 'Contact log could not be deleted. Please try again.' }
       end
     end
@@ -82,6 +91,11 @@ module TeamMembers
 
     private
 
+    def form_resources
+      @contact_types = ContactType.all
+      @users = User.all.order(email: :asc)
+    end
+
     def contact_log
       @contact_log = current_team_member.contact_logs.find(params[:id])
     rescue ActiveRecord::RecordNotFound
@@ -89,7 +103,7 @@ module TeamMembers
     end
 
     def contact_log_search
-      'lower(who_with) similar to lower(:query) or lower(what) similar to lower(:query)'
+      'lower(notes) similar to lower(:query)'
     end
 
     def past_contact_log_query
@@ -99,7 +113,7 @@ module TeamMembers
     end
 
     def contact_log_params
-      params.require(:contact_log).permit(:user, :notes, :contact_type, :start, :end)
+      params.require(:contact_log).permit(:user_id, :notes, :contact_type_id, :start, :end)
     end
 
     def failure
@@ -114,15 +128,10 @@ module TeamMembers
       redirect_back(fallback_location: new_contact_log_path, flash: { error: 'End date cannot be before start date' })
     end
 
-    def subheading_stats
-      @count_in_last_week = @resources.last_week.size
-      @count_in_last_month = @resources.last_month.size
-    end
-
     def set_breadcrumbs
       path = action_name == 'recent' ? nil : recent_contact_logs_path
       add_breadcrumb('My Contact logs', path, 'fas fa-clipboard-list')
-      add_breadcrumb('Past Contact logs') unless action_name != 'index'
+      add_breadcrumb('Archived Contact logs') unless action_name != 'index'
     end
   end
 end
