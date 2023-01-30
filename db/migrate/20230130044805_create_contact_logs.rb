d6e09adcbfc3badbf9cc5e7c19e05bc9456cf4a3
Tobias Sheer Hardwick <zaenilith@gmail.com>
2023-01-30 07:34:49 +0000

    Split out datepicker behaviour

--- db/migrate/20230130044805_create_contact_logs.rb
+++ db/migrate/20230130044805_create_contact_logs.rb
@@ -3,7 +3,7 @@ class CreateContactLogs < ActiveRecord::Migration[6.1]
     create_table :contact_logs do |t|
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :team_member, null: false, foreign_key: true
-      t.belongs_to :contact_types, null: false, foreign_key: true
+      t.belongs_to :contact_type, null: false, foreign_key: true
       t.datetime :start
       t.datetime :end
       t.string :notes
