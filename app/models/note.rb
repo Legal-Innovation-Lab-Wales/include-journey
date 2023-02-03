95fe4e5333ddc9733da3f8017de5bc1e41037bb5
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-02-03 04:17:34 +0000

    Add validation for optional_date

--- app/models/note.rb
+++ app/models/note.rb
@@ -5,7 +5,7 @@ class Note < ApplicationRecord
   belongs_to :replaced_by, class_name: 'Note', optional: true, foreign_key: 'replaced_by_id'
   belongs_to :replacing, class_name: 'Note', optional: true, foreign_key: 'replacing_id'
 
-  validates_presence_of :team_member_id, :user_id, :content
+  validates_presence_of :team_member_id, :user_id, :content, :optional_date
 
   def chain(array)
     array << self
