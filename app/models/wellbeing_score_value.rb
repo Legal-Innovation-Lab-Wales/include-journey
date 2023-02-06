4cfee5ebff4a6c4b38e46e9cf0802acc667eb7fd
Alaric Sheer Hardwick <43497938+Ricky656@users.noreply.github.com>
2023-02-06 15:16:34 +0000

    Added validation and error messages to appointments and wellbeing assessments

--- app/models/wellbeing_score_value.rb
+++ app/models/wellbeing_score_value.rb
@@ -1,3 +1,5 @@
 class WellbeingScoreValue < ApplicationRecord
     validates_format_of :name, with: Rails.application.config.regex_name
+    validates :value, numericality: { only_integer: true, greater_than: 0, less_than: 11,
+                                      message: 'Use sliders to set value' }
 end
--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -86,12 +86,14 @@ Rails.application.configure do
   config.regex_telephone = /\A[0-9+]*\z/
   config.regex_website = %r{\A[a-zA-Z0-9'\-._()/# ]*\z}
   config.regex_email = URI::MailTo::EMAIL_REGEXP
+  config.regex_datetime = /\A[a-zA-Z0-9+\-.:() ]*\z/
 
   config.text_field_error = 'Please only use standard characters and punctuation'
   config.name_error = 'Please only use alphanumeric characters'
   config.telephone_error = 'Please enter a valid telephone number'
   config.website_error = 'Please enter a valid website address'
   config.email_error = 'Please enter a valid email address'
+  config.datetime_error = 'Please select a valid date'
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
