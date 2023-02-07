723ce257aa8943887863fe41714330ddf8d8c21a
Tobias Sheer Hardwick <zaenilith@gmail.com>
2023-02-07 12:00:18 +0000

    Switched notes to using generic datetime_picker

--- app/javascript/packs/note_datetime_picker.js
+++
@@ -1,9 +0,0 @@
-const dated_when_field = document.querySelector('#dated_when'),
-    dated_time_field = document.querySelector('#dated_time'),
-    note_dated_hidden_field = document.querySelector('#note_dated'),
-    update_note_dated = () => note_dated_hidden_field.value = new Date(`${dated_when_field.value} ${dated_time_field.value}`)
-
-dated_when_field.addEventListener('change', update_note_dated)
-dated_time_field.addEventListener('change', update_note_dated)
-
-update_note_dated()
