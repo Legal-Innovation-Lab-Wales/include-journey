026b33af61fce024d08cc67ce2c035557eef7b61
Tobias Sheer Hardwick <zaenilith@gmail.com>
2023-02-07 11:21:57 +0000

    Corrected config names for reCAPTCHA

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -6,22 +6,6 @@ module Users
     skip_before_action :check_user, except: %i[new create]
     prepend_before_action :check_captcha, only: :create
 
-    private
-    def check_captcha
-      unless verify_recaptcha
-        self.resource = resource_class.new user_params
-        resource.validate
-        respond_with_navigational(resource) {
-          # delete default recaptcha error
-          flash.delete(:recaptcha_error)
-          # add custom recaptcha error
-          flash[:error] = "reCAPTCHA verification failed, please try again";
-          flash.discard(:error)
-          render :new 
-        }
-      end
-    end
-
     # DELETE /users
     def destroy
       current_user.update!(deleted_at: Time.now + 30.days)
@@ -34,8 +18,25 @@ module Users
     end
 
     private
+
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :mobile_number, :released_at, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :mobile_number, :released_at, :terms, :email, :password, :password_confirmation)
     end
+
+    def check_captcha
+      return unless verify_recaptcha
+
+      self.resource = resource_class.new user_params
+      resource.validate
+      respond_with_navigational(resource) {
+        # delete default recaptcha error
+        flash.delete(:recaptcha_error)
+        # add custom recaptcha error
+        flash[:error] = 'reCAPTCHA verification failed, please try again';
+        flash.discard(:error)
+        render :new
+      }
+    end
+
   end
 end
