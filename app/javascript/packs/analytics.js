554e804d181f04b5f2177e2a8b682f74c1f71f98
oluwatoyosi-swansea <o.a.oyegoke@swansea.ac.uk>
2023-02-09 13:54:29 +0000

    make admin dropdown label reactive

--- app/javascript/packs/analytics.js
+++ app/javascript/packs/analytics.js
@@ -13,6 +13,11 @@ data_select.addEventListener('change', function() {
         if (shared_with_select != null) {shared_with_select.style.display = 'none';}
         authored_by_select.style.display = 'block';
     } else {
+        if((data_select.value) == "Contact Logs"){
+            shared_with_select.previousElementSibling.innerHTML = "Created By";
+        }else {
+            shared_with_select.previousElementSibling.innerHTML = "Shared With";
+        }
         if (shared_with_select != null) {shared_with_select.style.display = 'block';}
         authored_by_select.style.display = 'none';
     }
