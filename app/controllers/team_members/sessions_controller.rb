4dd84a238eea386b16bb03f7a13c3cf8939ff481
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-02-09 14:51:51 +0000

    Include concern to sessions controller on create

--- app/controllers/team_members/sessions_controller.rb
+++ app/controllers/team_members/sessions_controller.rb
@@ -2,7 +2,14 @@ module TeamMembers
   # app/controllers/team_members/sessions_controller.rb
   class SessionsController < Devise::SessionsController
     include Accessible
+    include AuthenticateWithOtpTwoFactor
     skip_before_action :check_user, only: :destroy
+
+    prepend_before_action :authenticate_with_otp_two_factor,
+                        if: -> { action_name == 'create' && otp_two_factor_enabled? }
+
+    protect_from_forgery with: :exception, prepend: true, except: :destroy
+
     # before_action :configure_sign_in_params, only: [:create]
 
     # GET /resource/sign_in
@@ -28,7 +35,7 @@ module TeamMembers
     # end
 
     protected
-    
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_in, keys: [:otp_attempt])
     end
