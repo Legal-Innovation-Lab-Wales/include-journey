b65e09f25397b6a9c4ac455cc117464d8a37d933
oluwatoyosi-swansea <o.a.oyegoke@swansea.ac.uk>
2023-02-09 18:17:16 +0000

    add creation date, hide/show journal entries dropdown if team member is admin

--- app/javascript/packs/analytics.js
+++ app/javascript/packs/analytics.js
@@ -1,10 +1,17 @@
 const data_select = document.getElementById('data'),
       shared_with_select = document.getElementById('member'),
-      authored_by_select = document.getElementById('author');
+      authored_by_select = document.getElementById('author'),
+      member_parent = document.getElementById("member-parent");
+
+
+const isAdmin = member_parent.dataset.admin
 
 if ((data_select.value) == "Wellbeing Assessments") { 
     shared_with_select.style.display = 'none';  
 } else {
+    if(isAdmin === "false" && data_select.value !== "Contact Logs"){
+        member_parent.style.display = "none";
+    }
     authored_by_select.style.display = 'none';
 }
 
@@ -13,10 +20,18 @@ data_select.addEventListener('change', function() {
         if (shared_with_select != null) {shared_with_select.style.display = 'none';}
         authored_by_select.style.display = 'block';
     } else {
-        if((data_select.value) == "Contact Logs"){
-            shared_with_select.previousElementSibling.innerHTML = "Created By";
+        if(isAdmin === "false"){
+            if(data_select.value !== "Contact Logs"){
+                member_parent.style.display = "none";
+            } else {
+                member_parent.style.display = "block";
+            }
         }else {
-            shared_with_select.previousElementSibling.innerHTML = "Shared With";
+            if((data_select.value) == "Contact Logs"){
+                shared_with_select.previousElementSibling.innerHTML = "Created By";
+            }else {
+                shared_with_select.previousElementSibling.innerHTML = "Shared With";
+            }
         }
         if (shared_with_select != null) {shared_with_select.style.display = 'block';}
         authored_by_select.style.display = 'none';
