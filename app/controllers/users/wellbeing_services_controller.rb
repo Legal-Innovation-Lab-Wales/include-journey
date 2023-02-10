b7140b2d5d72f4313cd1cacfdab0fc1b6c2d7585
Alaric Sheer Hardwick <43497938+Ricky656@users.noreply.github.com>
2023-02-10 12:33:22 +0000

    Added basic pagination and search to 'my support'

--- app/controllers/users/wellbeing_services_controller.rb
+++ app/controllers/users/wellbeing_services_controller.rb
@@ -1,12 +1,25 @@
 module Users
   # app/controllers/users/wellbeing_services_controller.rb
   class WellbeingServicesController < UsersApplicationController
-    # GET /wellbeing_services
-    def index
-      add_breadcrumb('My Support', nil, 'fas fa-info-circle')
-      @wellbeing_services = WellbeingService.all
+    before_action :set_breadcrumbs
+    include Pagination
 
-      render 'index'
+    def resources
+      WellbeingService.all
     end
+
+    def resources_per_page
+      10
+    end
+
+    def set_breadcrumbs
+      add_breadcrumb('My Support', nil, 'fas fa-info-circle')
+    end
+
+    def search
+      WellbeingService.where('lower(name) similar to lower(:query)', wildcard_query)
+                      .order(created_at: :desc)
+    end
+
   end
 end
