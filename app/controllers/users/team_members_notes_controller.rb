2c2ba7a33653df5f1607d38c31ef0e395e0ba71f
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-03-06 21:44:56 +0000

    Create initial controller action and view

--- app/controllers/users/team_members_notes_controller.rb
+++ app/controllers/users/team_members_notes_controller.rb
@@ -1,4 +1,8 @@
 module Users
-  class TeamMembersNotesController < ApplicationController
+  # app/controllers/users/team_members_notes_controller.rb
+  class TeamMembersNotesController < UsersApplicationController
+    def index
+      @team_members_notes = Note.where(visible_to_user: true, user: current_user)
+    end
   end
 end
