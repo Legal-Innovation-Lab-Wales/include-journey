32eaf830980718933f7e0fbddc0902aff60dcb23
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-03-07 01:39:06 +0000

    Edit message page and implement breadcrumbs

--- app/controllers/users/team_members_notes_controller.rb
+++ app/controllers/users/team_members_notes_controller.rb
@@ -2,6 +2,7 @@ module Users
   # app/controllers/users/team_members_notes_controller.rb
   class TeamMembersNotesController < UsersApplicationController
     def index
+      add_breadcrumb('Message', nil, 'fas fa-envelope')
       @team_members_notes = Note.where(visible_to_user: true, user: current_user)
     end
   end
