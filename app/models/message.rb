2998188ea1250363b8dd4c833429c1e196e9ecc0
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-03-13 01:37:40 +0000

    Edit record validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,5 +3,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :team_member
 
-  validates_presence_of :user_id, :team_member_id, :identifier
+  validates_presence_of :user_id, :team_member_id, :note_id
 end
