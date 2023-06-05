0bab263f1056204e2bb2a94d064a6f4aa26af083
Alaric Sheer Hardwick <43497938+Ricky656@users.noreply.github.com>
2023-06-05 15:14:21 +0100

    Filtered search results to filter whether showing assigned or not

--- app/controllers/team_members/users_controller.rb
+++ app/controllers/team_members/users_controller.rb
@@ -132,6 +132,11 @@ module TeamMembers
                      .order({ "#{@sort}": @direction })
       end
       @users = users_params[:tag].present? ? @users.joins(:user_tags).where('user_tags.tag_id': users_params[:tag]) : @users
+      if users_params[:assigned].present? && users_params[:assigned] == 'false'
+        @users
+      else
+        @users.joins(:assignments).where('assignments.team_member': current_team_member)
+      end
     end
 
     def resources_per_page
