403089ca3ae0ac2ec9814255f3ae7601826e3e24
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-07-22 05:35:17 +0100

    Edit migration file name

--- db/migrate/20230719070625_create_upload_files.rb
+++ db/migrate/20230719070625_create_upload_files.rb
@@ -1,6 +1,6 @@
-class CreatePhotos < ActiveRecord::Migration[6.1]
+class CreateUploadFiles < ActiveRecord::Migration[6.1]
   def change
-    create_table :photos do |t|
+    create_table :upload_files do |t|
       t.binary :data
       t.references :upload, null: false, foreign_key: true
 
