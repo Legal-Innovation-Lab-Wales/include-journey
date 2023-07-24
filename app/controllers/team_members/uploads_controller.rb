875bd1a4284431e8fb12e373598859c19f3a7994
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-07-24 09:24:54 +0100

    Do team member upload new, create, index

--- app/controllers/team_members/uploads_controller.rb
+++ app/controllers/team_members/uploads_controller.rb
@@ -2,27 +2,27 @@ module TeamMembers
   # app/controllers/team_members/upload_controller.rb
   class UploadsController < ApplicationController
     before_action :set_breadcrumbs
-
-    def index
-      @uploads = Upload.where(user: user)
-    end
+    before_action :user
+    before_action :upload, only: %i[new create edit update destroy download_file]
+    include Pagination
 
     def new
       add_breadcrumb('New Upload', nil, 'fas fa-plus-circle')
-      @upload = Upload.new
+      @upload = @user.uploads.new
       @upload_file = UploadFile.new
     end
 
     def create
-      @upload = Upload.new(
-        comment: upload_params[comment],
-        user: user
-      )
+      @upload = Upload.new(comment: upload_params[:comment],
+                           user: @user)
       @upload.uploadable = current_team_member
+
       @upload_file = new_upload_file
+      @upload_file.upload = @upload
 
       if @upload.save! && @upload_file.save!
         flash[:success] = 'Upload added successfully!'
+        redirect_back(fallback_location: root_path)
       else
         render 'new', status: :unprocessable_entity
       end
@@ -44,14 +44,57 @@ module TeamMembers
       end
     end
 
+    def download_file
+      @upload_file = @upload.upload_file
+      pdf_blob = @upload_file.data
+
+      case @upload_file.content_type
+      when 'application/pdf'
+        send_data pdf_blob, filename: @upload_file.name, type: 'application/pdf', disposition: 'attachment'
+      when 'image/jpeg'
+        send_data pdf_blob, filename: @upload_file.name, type: 'image/jpeg', disposition: 'attachment'
+      when 'image/png'
+        send_data pdf_blob, filename: @upload_file.name, type: 'image/png', disposition: 'attachment'
+      else
+        flash[:alert] = 'Something went wrong!'
+      end
+    end
+
+    def subheading_stats
+      return unless @resources.present?
+
+      @uploads_in_last_30_days = @resources.created_in_last_month.count
+      @image_uploads = @resources.images.count
+      @pdf_uploads = @resources.pdf_files.count
+      @team_member_uploads = @resources.uploaded_by_teammember.count
+      @user_uploads = @resources.uploaded_by_user.count
+    end
+
+    protected
+
+    def resources
+      Upload.where(uploadable_type: 'TeamMember').or(Upload.where(uploadable_type: 'User'))
+            .joins(:upload_file)
+    end
+
+    def resources_per_page
+      9
+    end
+
+    def search
+      Upload.where(uploadable_type: 'TeamMember').or(Upload.where(uploadable_type: 'User'))
+            .where(user_id: @user.id).joins(:upload_file)
+            .where(upload_search, wildcard_query).order(created_at: :desc)
+    end
+
     private
 
     def upload_params
-      params.require(:upload).permit(:comments, :image_file, :cached_image)
+      params.require(:upload).permit(:comment, :file, :cached_file, :content_type, :name)
     end
 
     def user
-      User.find(params[:user_id])
+      @user = User.includes(:uploads).find(ActiveRecord::Base::sanitize_sql_for_conditions(params[:user_id]))
     end
 
     def upload
@@ -63,14 +106,17 @@ module TeamMembers
     end
 
     def new_upload_file
-      upload_file = UploadFile.new
-      upload_file.data = if upload_params[:cached_image]
-                     encode(upload_params[:cached_image])
-                   elsif upload_params[:image_file]
-                     upload_params[:image_file].read
-                   end
-      upload_file.upload = @upload
-      upload_file
+      UploadFile.new(name: upload_params[:name],
+                     content_type: upload_params[:file].content_type,
+                     data: if upload_params[:cached_file]
+                             encode(upload_params[:cached_file])
+                           elsif upload_params[:file]
+                             upload_params[:file].read
+                           end)
+    end
+
+    def upload_search
+      'lower(upload_files.name) similar to lower(:query)'
     end
 
     def set_breadcrumbs
