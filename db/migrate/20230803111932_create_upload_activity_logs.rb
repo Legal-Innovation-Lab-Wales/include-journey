2bcb9d5453a60a74aa5d252714c2346fae653583
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-08-03 12:42:17 +0100

    Enhance UAL's migration

--- db/migrate/20230803111932_create_upload_activity_logs.rb
+++ db/migrate/20230803111932_create_upload_activity_logs.rb
@@ -3,6 +3,12 @@ class CreateUploadActivityLogs < ActiveRecord::Migration[6.1]
     create_table :upload_activity_logs do |t|
       t.string :activity_type
       t.datetime :activity_time
+      t.integer :create_count, default: 0
+      t.integer :view_count, default: 0
+      t.integer :modify_count, default: 0
+      t.integer :download_count, default: 0
+      t.integer :approve_count, default: 0
+      t.integer :reject_count, default: 0
       t.references :team_members, null: false, foreign_key: true
       t.references :uploads, null: false, foreign_key: true
 
