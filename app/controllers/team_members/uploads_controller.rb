b309372b9d7d53b063ae941b0024aecf1b665314
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-08-11 17:24:52 +0100

    Limit file size 5MB

--- app/controllers/team_members/uploads_controller.rb
+++ app/controllers/team_members/uploads_controller.rb
@@ -31,7 +31,7 @@ module TeamMembers
       @upload_file = new_upload_file
       @upload_file.upload = @upload
 
-      max_file_size = 250.megabytes
+      max_file_size = 5.megabytes
       if @upload_file.data.size > max_file_size
         flash[:error] = 'File size exceeds the maximum limit of 250MB.'
         render 'new', status: :unprocessable_entity
