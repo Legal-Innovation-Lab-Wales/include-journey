2dce9c287ae1c80de2c61dd93ad46998ee67a86f
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-08-29 10:04:53 +0100

    Fix bug

--- db/migrate/20230826140654_create_emergency_contact.rb
+++ db/migrate/20230826140654_create_emergency_contact.rb
@@ -3,7 +3,7 @@ class CreateEmergencyContact < ActiveRecord::Migration[6.1]
     create_table :emergency_contacts do |t|
       t.string :name
       t.string :relationship
-      t.integer :number
+      t.bigint :number
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
