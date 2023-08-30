282c01d70ab2f1f8e92fc042b5353864141d0558
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-08-30 19:28:50 +0100

    Add validation checks to new db tables

--- app/models/local_authority.rb
+++ app/models/local_authority.rb
@@ -1,4 +1,16 @@
 # app/models/local_authority.rb
 class LocalAuthority < ApplicationRecord
   has_one :user
+
+  validates_presence_of :name
+
+  def json
+    {
+      'name': name
+    }
+  end
+
+  def to_csv
+    [name]
+  end
 end
