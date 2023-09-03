ec36406a5c5526bfcc533edbc039533c0079e4af
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-09-03 21:39:31 +0100

    Fix db bug

--- db/seeds/37_wallich_local_authorities.rb
+++ db/seeds/37_wallich_local_authorities.rb
@@ -1,9 +1,9 @@
-if LocalAuthority.count.zero?
+if WallichLocalAuthority.count.zero?
   print "#{pretty_print_name('Local Authorities')}\tStart: #{pretty_print(Time.now - @start_time)}"
 
-  LocalAuthority.create!(name: 'Swan')
-  LocalAuthority.create!(name: 'NPT')
-  LocalAuthority.create!(name: 'Other')
+  WallichLocalAuthority.create!(name: 'Swan')
+  WallichLocalAuthority.create!(name: 'NPT')
+  WallichLocalAuthority.create!(name: 'Other')
 
   puts "\tDuration: #{pretty_print(Time.now - @last_time)}   Elapsed: #{pretty_print(Time.now - @start_time)}"
   @last_time = Time.now
