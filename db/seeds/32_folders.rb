4ceff3868f7e2f32ea1d1975f7e290dd902c35a8
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-09-08 03:51:29 +0100

    add more folder seed data

--- db/seeds/32_folders.rb
+++ db/seeds/32_folders.rb
@@ -1,11 +1,15 @@
 if Folder.count.zero?
   print "#{pretty_print_name('Folders')}\tStart: #{pretty_print(Time.now - @start_time)}"
 
-  5.times do
+  30.times do
     top_folder = Folder.create!(name: Faker::Name.first_name)
-    5.times do
-      Folder.create!(name: Faker::Name.first_name,
-                     parent_folder: top_folder)
+    30.times do
+      child_folder1 = Folder.create!(name: Faker::Name.first_name,
+                                     parent_folder: top_folder)
+      5.times do
+        Folder.create!(name: Faker::Name.first_name,
+                       parent_folder: child_folder1)
+      end
     end
   end
 
