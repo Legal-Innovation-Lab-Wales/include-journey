3d1adb1e1c4075f013179264b563522ad17ee814
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-10-04 13:51:11 +0100

    initial set up for user notification feature

---
+++ app/controllers/users/notifications_controller.rb
@@ -0,0 +1,8 @@
+module Users
+  # app/controllers/users/notifications_controller.rb
+  class NotificationsController < UsersApplicationController
+    def index
+      render 'index'
+    end
+  end
+end
--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,6 +1,26 @@
 # app/helpers/notifications_helper.rb
 module NotificationsHelper
-  def alert_user_of_notifications?
+  def alert_team_member_of_notifications?
     current_team_member && current_team_member.notifications.where(viewed: false, upload_id: nil).count.positive?
+  end
+
+  def alert_user_of_notification?
+    current_user && (new_messages_for_user? || new_uploads_for_user?)
+  end
+
+  def user_messages_count
+    current_user.messages.where(read: false).count
+  end
+
+  def user_uploads_count
+    current_user.notifications.where.not(upload: nil, viewed: false).count
+  end
+
+  def new_messages_for_user?
+    current_user.messages.where(read: false).count.positive?
+  end
+
+  def new_uploads_for_user?
+    current_user.notifications.where.not(upload: nil, viewed: false).count.positive?
   end
 end
