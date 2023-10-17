2ab9f50399cad19b2a606bdebbe56f79c8820ef0
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-10-17 04:09:57 +0100

    Edit migration file

--- db/migrate/20231016193215_create_team_member_notification_frequency.rb
+++ db/migrate/20231016193215_create_team_member_notification_frequency.rb
@@ -3,7 +3,6 @@ class CreateTeamMemberNotificationFrequency < ActiveRecord::Migration[6.1]
     create_table :team_member_notification_frequencies do |t|
       t.string :accommodation_status, default: '6 months'
       t.string :wellbeing_assessment, default: '3 months'
-      t.references :team_member, null: false, foreign_key: true
 
       t.timestamps
     end
