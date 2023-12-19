c58b9f5c36d2b68efda5461090221d0515acf550
Amara Finbarrs-Ezema <amarafinbarrs123@gmail.com>
2023-12-19 06:28:25 +0000

    Add validations

--- app/models/occupational_therapist_metric.rb
+++ app/models/occupational_therapist_metric.rb
@@ -1,3 +1,7 @@
 class OccupationalTherapistMetric < ApplicationRecord
   has_many :ota_entries
+
+  validates_presence_of :name
+  validates_format_of :name, with: Rails.application.config.regex_name,
+                             message: Rails.application.config.name_error
 end
--- app/models/occupational_therapist_score.rb
+++ app/models/occupational_therapist_score.rb
@@ -1,3 +1,15 @@
 class OccupationalTherapistScore < ApplicationRecord
   has_many :ota_entries
+
+  OPTIONS = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    'N/A'
+  ].freeze
+
+  validates_presence_of :value
+  validates :value, inclusion: { in: OPTIONS }
 end
