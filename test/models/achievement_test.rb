61680661e104edacedcc43727be65b059b1b3640
Andrew Kay <a.kay@swansea.ac.uk>
2025-02-05 11:59:18 +0000

    Add tests for user_achievements

--- test/models/achievement_test.rb
+++ test/models/achievement_test.rb
@@ -3,6 +3,8 @@
 require 'test_helper'
 
 class AchievementTest < ActiveSupport::TestCase
+  fixtures :achievements, :users
+
   test 'achievement without required fields is invalid' do
     achievement = Achievement.new
 
@@ -44,5 +46,124 @@ class AchievementTest < ActiveSupport::TestCase
     assert_equal 10, achievement.bronze_count
     assert_equal 20, achievement.silver_count
     assert_equal 50, achievement.gold_count
+  end
+
+  test 'user has all-time achievement medals after correct number of sessions' do
+    # Achievement for number of sessions
+    achievement = achievements(:familiar_face)
+    user = users(:alice)
+    a = achievement.user_achievements.find_or_create_by!(user: user)
+
+    # achievement.check mutates the user_achievement in the database, so must reload
+    achievement.check(user)
+    a.reload
+    assert_equal [false, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # No medals after 99 sessions
+    add_sessions(user, nil, 0...99)
+    achievement.check(user)
+    a.reload
+    assert_equal [false, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # Bronze after 100 sessions
+    add_sessions(user, nil, 99...100)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # No silver after 249 sessions
+    add_sessions(user, nil, 100...249)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # Silver after 250 sessions
+    add_sessions(user, nil, 249...250)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, true, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # No gold after 499 sessions
+    add_sessions(user, nil, 250...499)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, true, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # Gold after 500 sessions
+    add_sessions(user, nil, 499...500)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, true, true], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+  end
+
+  test 'user has monthly achievement medals after correct number of sessions within date range' do
+    # Achievement for number of sessions
+    achievement = achievements(:familiar_face_jan)
+    user = users(:alice)
+    a = achievement.user_achievements.find_or_create_by!(user: user)
+
+    # achievement.check mutates the user_achievement in the database, so must reload
+    achievement.check(user)
+    a.reload
+    assert_equal [false, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # No medals after 9 sessions
+    add_sessions(user, achievement.starts_at, 0...9)
+    achievement.check(user)
+    a.reload
+    assert_equal [false, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # Bronze after 10 sessions
+    add_sessions(user, achievement.starts_at, 9...10)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # No silver after 19 sessions
+    add_sessions(user, achievement.starts_at, 10...19)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # Silver after 20 sessions
+    add_sessions(user, achievement.starts_at, 19...20)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, true, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # No gold after 30 sessions
+    add_sessions(user, achievement.starts_at, 20...30)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, true, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+
+    # Gold after 31 sessions
+    add_sessions(user, achievement.starts_at, 30...31)
+    achievement.check(user)
+    a.reload
+    assert_equal [true, true, true], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+  end
+
+  test 'user is not awarded monthly achievement medals for sessions outside of date range' do
+    skip "This is currently not implemented correctly"
+
+    # Achievement for number of sessions
+    achievement = achievements(:familiar_face_jan)
+    user = users(:alice)
+    a = achievement.user_achievements.find_or_create_by!(user: user)
+
+    add_sessions(user, achievement.starts_at - 1.year, 0...31)
+    achievement.check(user)
+    a.reload
+    assert_equal [false, false, false], [a.bronze_achieved, a.silver_achieved, a.gold_achieved]
+  end
+
+  private
+
+  def add_sessions(user, start_at, indices)
+    start_at ||= Date.new(2020, 1, 1)
+    Session.create(indices.map do |index|
+      {user: user, session_at: start_at + index.days}
+    end)
   end
 end
--- test/models/affirmation_test.rb
+++ test/models/affirmation_test.rb
@@ -15,7 +15,7 @@ class AffirmationTest < ActiveSupport::TestCase
     affirmation = Affirmation.new
     assert_invalid affirmation, :text
     assert_invalid affirmation, :scheduled_date
-    assert_invalid affirmation, :team_member_id
+    assert_invalid affirmation, :team_member
   end
 
   test 'affirmation with empty text is invalid' do
